cf335229c7cb50b33cf2fa911cbaed4be17f7ffe
rickutino <rickutino@gmail.com>
2021-02-03 20:31:40 +0900

    add orphanages page routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing/index';
+import OrphanagesMap from '../pages/OrphanagesMap/index';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Landing} />
+    <Route path="/orphanages" component={OrphanagesMap} />
   </Switch>
 );
 export default Routes;
