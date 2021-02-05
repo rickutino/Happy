ba1f15a917fc2f2f7bf6af596a6187301d025c90
rickutino <rickutino@gmail.com>
2021-02-06 00:02:27 +0900

    add action create route to orphanages

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,41 @@
+/* eslint-disable camelcase */
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  response.json({ message: 'Hello World!' });
+  return response.json({ message: 'Hello World!' });
 });
+
+routes.post('/orphanages', async (request, response) => {
+  console.log(request.body);
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
+});
+
 export default routes;
