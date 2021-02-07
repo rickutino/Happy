1b867af07b7723bfa950395646a7958d0970a411
rickutino <rickutino@gmail.com>
2021-02-07 22:54:34 +0900

    add error handle

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/index';
+import errorHandler from './errors/handle';
 
 import routes from './routes';
 
@@ -10,7 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
 });
