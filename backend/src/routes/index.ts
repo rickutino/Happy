beb1fb7ad7b7872370881ca49d10fce467005885
rickutino <rickutino@gmail.com>
2021-02-07 13:42:39 +0900

    config upload imagens

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import OrphanageController from '../controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
@@ -9,6 +13,6 @@ routes.get('/', (request, response) => {
 
 routes.get('/orphanages', OrphanageController.index);
 routes.get('/orphanages/:id', OrphanageController.show);
-routes.post('/orphanages', OrphanageController.create);
+routes.post('/orphanages', upload.array('images'), OrphanageController.create); // upload.single() para uma imagem a ser salva
 
 export default routes;
