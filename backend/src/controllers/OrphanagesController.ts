4eae638c21370b46c58b9e78a2257d754c25d222
rickutino <rickutino@gmail.com>
2021-02-07 15:18:59 +0900

    return show images of GET: /:id

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -20,7 +20,9 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
     return response.json(orphanage);
   },
