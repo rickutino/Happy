62951f32ffbe42ee0b51e5a3dc5ee8135e5f3d81
rickutino <rickutino@gmail.com>
2021-02-21 13:29:19 +0900

    fix cors position

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,10 +11,10 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-app.use(cors());
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
